470854b9e7a98b2d1541fe64ead0ce6d8fab9c37
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2022-12-20 15:56:27 -0500

    feat: disabled tooltip

---
+++ src/components/ResumeTooltip.vue
@@ -0,0 +1,16 @@
+<template>
+
+	<v-tooltip activator='parent' v-if='paused' location='top center' class='tooltip-z'>
+		resume to alter
+	</v-tooltip>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	paused: {
+		type: Boolean,
+		required: true
+	}
+});
+
+</script>
