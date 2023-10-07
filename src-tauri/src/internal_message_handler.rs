e742c5d11038efb40af2dadc6875df9f31a8ef9e
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-10-07 13:28:36 +0100

    feat: single instance
    Refactor window control into own mod

--- src-tauri/src/internal_message_handler.rs
+++ src-tauri/src/internal_message_handler.rs
@@ -11,7 +11,7 @@ use crate::{
     request_handlers::{EmitMessages, ShowTimer},
     system_tray::{menu_enabled, MenuItem},
     tick::tick_process,
-    ObliqoroWindow,
+    ObliqoroWindow, window_action::WindowAction,
 };
 use tokio::sync::broadcast::{Receiver, Sender};
 
@@ -58,6 +58,7 @@ pub enum WindowVisibility {
     Hide,
     Minimize,
     Toggle,
+	Show,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Hash)]
@@ -85,72 +86,79 @@ pub enum InternalMessage {
 }
 
 /// Control the frontend window component visibility
-struct WindowAction;
-impl WindowAction {
-    /// Show the window
-    /// Linux v Windows, need to handle fullscreen & resize on each platform differently
-    #[cfg(target_os = "windows")]
-    fn show(window: &tauri::Window, fullscreen: bool) {
-        window.set_fullscreen(fullscreen).ok();
-        window.set_resizable(false).ok();
-        window.show().ok();
-        window.center().ok();
-    }
+// struct WindowAction;
+// impl WindowAction {
+//     /// Show the window
+//     /// Linux v Windows, need to handle fullscreen & resize on each platform differently
+//     #[cfg(target_os = "windows")]
+//     fn show(window: &tauri::Window, fullscreen: bool) {
+//         window.set_fullscreen(fullscreen).ok();
+//         window.set_resizable(false).ok();
+//         window.show().ok();
+//         window.center().ok();
+//     }
 
-    /// Show the window
-    /// see github issue #1
-    #[cfg(not(target_os = "windows"))]
-    fn show(window: &tauri::Window, fullscreen: bool) {
-        if fullscreen {
-            if window.is_visible().unwrap_or_default() {
-                window.hide().ok();
-            }
-            window.set_resizable(true).ok();
-            window.set_fullscreen(fullscreen).ok();
-            // This is the linux fix - dirty, but it seems to work
-            std::thread::sleep(std::time::Duration::from_millis(50));
-        } else if window.is_resizable().unwrap_or(false) {
-            window.set_resizable(false).ok();
-        }
-        window.show().ok();
-        window.center().ok();
-    }
+//     /// Show the window
+//     /// see github issue #1
+//     #[cfg(not(target_os = "windows"))]
+//     fn show(window: &tauri::Window, fullscreen: bool) {
+//         if fullscreen {
+//             if window.is_visible().unwrap_or_default() {
+//                 window.hide().ok();
+//             }
+//             window.set_resizable(true).ok();
+//             window.set_fullscreen(fullscreen).ok();
+//             // This is the linux fix - dirty, but it seems to work
+//             std::thread::sleep(std::time::Duration::from_millis(50));
+//         } else if window.is_resizable().unwrap_or(false) {
+//             window.set_resizable(false).ok();
+//         }
+//         window.show().ok();
+//         window.center().ok();
+//     }
 
-    /// Hide window
-    fn hide(window: &tauri::Window, fullscreen: bool) {
-        if fullscreen {
-            window.set_resizable(true).ok();
-            window.set_fullscreen(false).ok();
-        }
-        window.hide().ok();
-        window.center().ok();
-    }
+//     /// Hide window
+//     fn hide(window: &tauri::Window, fullscreen: bool) {
+//         if fullscreen {
+//             window.set_resizable(true).ok();
+//             window.set_fullscreen(false).ok();
+//         }
+//         window.hide().ok();
+//         window.center().ok();
+//     }
 
-    /// hide window
-    pub fn hide_window(app: &AppHandle, fullscreen: bool) {
-        if let Some(window) = app.get_window(ObliqoroWindow::Main.as_str()) {
-            Self::hide(&window, fullscreen);
-        }
-    }
+// 	/// show window
+//     pub fn show_window(app: &AppHandle, fullscreen: bool) {
+//         if let Some(window) = app.get_window(ObliqoroWindow::Main.as_str()) {
+//             Self::show(&window, fullscreen);
+//         }
+//     }
 
-    /// Toggle the visible of the main window based on current visibility
-    pub fn toggle_visibility(app: &AppHandle, fullscreen: bool) {
-        if let Some(window) = app.get_window(ObliqoroWindow::Main.as_str()) {
-            match window.is_visible() {
-                Ok(true) => Self::hide(&window, fullscreen),
-                Ok(false) => Self::show(&window, fullscreen),
-                Err(_) => app.exit(1),
-            }
-        }
-    }
+//     /// hide window
+//     pub fn hide_window(app: &AppHandle, fullscreen: bool) {
+//         if let Some(window) = app.get_window(ObliqoroWindow::Main.as_str()) {
+//             Self::hide(&window, fullscreen);
+//         }
+//     }
 
-    // unminimize the main window
-    // pub fn unminimize(app: &AppHandle) {
-    //     if let Some(window) = app.get_window(ObliqoroWindow::Main.as_str()) {
-    //         window.unminimize().unwrap_or_default();
-    //     }
-    // }
-}
+//     /// Toggle the visible of the main window based on current visibility
+//     pub fn toggle_visibility(app: &AppHandle, fullscreen: bool) {
+//         if let Some(window) = app.get_window(ObliqoroWindow::Main.as_str()) {
+//             match window.is_visible() {
+//                 Ok(true) => Self::hide(&window, fullscreen),
+//                 Ok(false) => Self::show(&window, fullscreen),
+//                 Err(_) => app.exit(1),
+//             }
+//         }
+//     }
+
+//     // unminimize the main window
+//     // pub fn unminimize(app: &AppHandle) {
+//     //     if let Some(window) = app.get_window(ObliqoroWindow::Main.as_str()) {
+//     //         window.unminimize().unwrap_or_default();
+//     //     }
+//     // }
+// }
 
 // Update the taskbar to display how many sessions before next long break,
 // and send internal message, to send message to front end to update settings in pinia
@@ -311,12 +319,12 @@ fn handle_visibility(
                 WindowAction::hide_window(app, false);
             }
         }
+	
         WindowVisibility::Minimize => {
-            // if on_break {
-            //     WindowAction::unminimize(app);
-            // } else {
             WindowAction::hide_window(app, false);
-            // }
+        }
+		WindowVisibility::Show => {
+			WindowAction::show_window(app, false);
         }
         WindowVisibility::Toggle => {
             if !on_break {
@@ -440,16 +448,12 @@ fn handle_break(
             state.lock().start_break_session();
             menu_enabled(app, false);
             sx.send(InternalMessage::Emit(Emitter::Timer)).ok();
-            if let Some(window) = app.get_window(ObliqoroWindow::Main.as_str()) {
-                WindowAction::show(&window, fullscreen);
-            }
+			WindowAction::show_window(app, fullscreen);
         }
         BreakMessage::End => {
             state.lock().start_work_session();
             menu_enabled(app, true);
-            if let Some(window) = app.get_window(ObliqoroWindow::Main.as_str()) {
-                WindowAction::hide(&window, fullscreen);
-            }
+			WindowAction::hide_window(app, fullscreen);
             update_menu(app, state, sx);
         }
     }
