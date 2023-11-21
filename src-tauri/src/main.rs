99e9ff212869e2ce893cf72402042d1b2c06184c
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-11-21 11:16:20 +0000

    chore: lints moved from main.rs to Cargo.toml

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,3 @@
-#![forbid(unsafe_code)]
-#![warn(
-    clippy::expect_used,
-    clippy::nursery,
-    clippy::pedantic,
-    clippy::todo,
-    clippy::unused_async,
-    clippy::unwrap_used
-)]
-#![allow(clippy::module_name_repetitions, clippy::doc_markdown)]
 // Only allow when debugging
 // #![allow(unused)]
 #![cfg_attr(
